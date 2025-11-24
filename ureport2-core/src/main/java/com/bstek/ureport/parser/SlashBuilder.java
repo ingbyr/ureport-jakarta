66f726cf47674d53d2b6ad0add5eef8272050ada
zhangwenkai <zhangwenkai2@ccccltd.cn>
2025-11-24 14:17:49 +0800

    support java21

--- ureport2-core/src/main/java/com/bstek/ureport/parser/SlashBuilder.java
+++ ureport2-core/src/main/java/com/bstek/ureport/parser/SlashBuilder.java
@@ -24,12 +24,12 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.imageio.stream.MemoryCacheImageOutputStream;
 
-import org.springframework.util.Base64Utils;
 
 import com.bstek.ureport.cache.ResourceCache;
 import com.bstek.ureport.definition.CellDefinition;
@@ -190,7 +190,7 @@ public class SlashBuilder {
 		try{
 			ImageIO.write(image, "png", memoryImage);
 			imageBytes=byteOutput.toByteArray();
-			String base64Data=Base64Utils.encodeToString(imageBytes);
+			String base64Data= Base64.getEncoder().encodeToString(imageBytes);
 			content.setBase64Data(base64Data);
 		}catch(Exception ex){
 			throw new ReportComputeException(ex);
